50d6ea285ba570f630199e0142801f75547dff7d
montakb1 <100643338+montakb1@users.noreply.github.com>
2024-11-10 15:26:24 +0100

    Create Animal.java

---
+++ prosit2/Animal.java
@@ -0,0 +1,60 @@
+package prosit2;
+
+public class Animal {
+    private String family;
+    private String name;
+    private int age;
+    private boolean isMammal;
+
+    // Constructeur paramétré
+    public Animal(String family, String name, int age, boolean isMammal) {
+        this.family = family;
+        this.name = name;
+        this.age = age;
+        this.isMammal = isMammal;
+    }
+
+    // Getters
+    public String getFamily() {
+        return family;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isMammal() {
+        return isMammal;
+    }
+
+    // Setters
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setMammal(boolean mammal) {
+        isMammal = mammal;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "family='" + family + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", isMammal=" + isMammal +
+                '}';
+    }
+}
