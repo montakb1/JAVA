a82bb9dcaad7ec413210e02e9c10649fd3b64fb6
montakb1 <100643338+montakb1@users.noreply.github.com>
2024-11-10 16:07:22 +0100

    Create Dolphin.java

---
+++ prosit5/tn/esprit/gestionzoo/entities/Dolphin.java
@@ -0,0 +1,37 @@
+package gestionzoo.entities;
+
+public class Dolphin extends Aquatic {
+    private float swimmingSpeed;
+
+    public Dolphin() {
+        super();
+    }
+
+    public Dolphin(String family, String name, int age, boolean isMammal, String habitat, float swimmingSpeed) {
+        super(family, name, age, isMammal, habitat);
+        setSwimmingSpeed(swimmingSpeed);
+    }
+
+    public float getSwimmingSpeed() {
+        return swimmingSpeed;
+    }
+
+    public void setSwimmingSpeed(float swimmingSpeed) {
+        if (swimmingSpeed < 0) {
+            throw new IllegalArgumentException("La vitesse de nage ne peut pas être négative");
+        }
+        this.swimmingSpeed = swimmingSpeed;
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("This dolphin is swimming.");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Dolphin{" +
+                "swimmingSpeed=" + swimmingSpeed +
+                '}';
+    }
+}
